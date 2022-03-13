52b715e747d53cf8953a449b80943c78bedf192a
ARod <ajr.rodriguez095@gmail.com>
2022-03-12 21:19:34 -0500

    employee class test passed

--- Develop/lib/Engineer.js
+++ Develop/lib/Engineer.js
@@ -1,1 +1,23 @@
+const Employee = require('./Employee');
+
 // TODO: Write code to define and export the Engineer class.  HINT: This class should inherit from Employee.
+class Engineer extends Employee {
+    
+    constuctor(github) {
+       this.github = github;
+    }
+
+    getGithub() {
+        return {  
+
+        }
+    };
+
+    getRole() {
+        return {
+
+        }
+    };
+};
+
+module.exports = Engineer;
