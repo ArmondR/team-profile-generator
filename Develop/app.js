e9a375716b102b8fc0c008338ce8fb068859517b
ARod <ajr.rodriguez095@gmail.com>
2022-04-03 22:02:50 -0400

    working towards html generation functionality

--- Develop/app.js
+++ Develop/app.js
@@ -189,16 +189,32 @@ inquirer
                     if(newEmployee) {
                         chooseEmployee(employeesArr);
                     } else {
-                        return employeesArr;
+                        employeesArr;
                     }
-                }) 
+                })
+                .then(employeesArr => {
+                    return render(employeesArr);
+                })
+                .then(generatedHtml => {
+                    return writeFile(generatedHtml);
+                })
+                .catch(err => {
+                    console.log(err);
+                });
             };
-                
+
+            const writeFile = data => {
+                fs.writeFile(outputPath, data, err => {
+                    if (err) {
+                        console.log(err);
+                        return;
+                    } else {
+                        console.log('html generated')
+                    }
+                })
+            }
+            
                     
-
-        
-
-
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
 // generate and return a block of HTML including templated divs for each employee!
